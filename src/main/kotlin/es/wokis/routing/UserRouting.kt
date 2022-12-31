cde89a365b9f9e9c6af70126c48620f6e0d7548f
Wikijito7 <wikyfg@gmail.com>
2022-12-31 20:07:13 +0100

    Added /user

--- src/main/kotlin/es/wokis/routing/UserRouting.kt
+++ src/main/kotlin/es/wokis/routing/UserRouting.kt
@@ -2,6 +2,8 @@ package es.wokis.routing
 
 import es.wokis.data.mapper.user.toDTO
 import es.wokis.data.repository.user.UserRepository
+import es.wokis.utils.user
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
 import io.ktor.server.response.*
@@ -14,6 +16,19 @@ fun Routing.setUpUserRouting() {
         get("/users") {
             val users = userRepository.getUsers().toDTO()
             call.respond(users)
+        }
+
+        authenticate {
+            get("/user") {
+                val callUser = call.user
+                callUser?.id?.let {
+                    val user = userRepository.getUserById(it)
+                    user?.let {
+                        call.respond(HttpStatusCode.OK, user.toDTO())
+                    } ?: call.respond(HttpStatusCode.NotFound, callUser)
+
+                } ?: call.respond(HttpStatusCode.Unauthorized)
+            }
         }
     }
 }
